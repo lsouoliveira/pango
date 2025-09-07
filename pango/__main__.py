e6eb2cc4dec316f9b868e9dde27c99ed0583c3df
Lucas Sousa <30642647+lsouoliveira@users.noreply.github.com>
2025-09-07 09:22:36 -0300

    Add model to classify puzzle symbols

--- pango/__main__.py
+++ pango/__main__.py
@@ -1,7 +1,7 @@
 from pango.puzzle_image_solver import PuzzleImageSolverPipeline
 
 if __name__ == "__main__":
-    solver = PuzzleImageSolverPipeline.load_image("data/sample_03.png")
+    solver = PuzzleImageSolverPipeline.load_image("data/train.png")
     _, puzzle = solver.run()
 
     print(puzzle.to_json())
